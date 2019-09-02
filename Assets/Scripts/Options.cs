54d21478d3f9d6b27013473897fa484a1c41bb0b
Dane <danemsherman@gmail.com>
2019-09-02 15:39:42 -0400

    Version 0.1

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    /// <summary>
+    /// handles the UI for creating a new Task
+    /// </summary>
+
+    [SerializeField] private TaskList taskList;
+    [SerializeField] private ScreenController screenControl;
+
+    [Space(10)]
+    [SerializeField] private InputField nameInput;
+    [SerializeField] private MonthSelect monthSelect;
+    [SerializeField] private DaySelect daySelect;
+    [SerializeField] private TagSelect tagSelect;
+
+    /// <summary>
+    /// Called by the DONE button to create a task
+    /// </summary>
+    public void Done()
+    {
+        //check if the input is valid
+        if (Validate(nameInput.text, tagSelect.SelectedTag))
+        {
+            //add the task to the list
+            taskList.AddTask(nameInput.text, 
+                daySelect.SelectedDay, monthSelect.SelectedMonth, monthSelect.SelectedYear,
+                tagSelect.SelectedTag.name);
+            screenControl.ShowTaskScreen();
+
+            taskList.SaveData();
+        }
+    }
+
+    public bool Validate(string name, Tag tag)
+    {
+        if(name.Length == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
